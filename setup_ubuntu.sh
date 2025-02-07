ad7bbcdb5c6d5ffd392ea324f35da18dd116a4b3
yanndriann <dimssyooo01@gmail.com>
2025-02-07 23:02:29 +0700

    setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Aktifkan mode superuser
-sudo -i
+# Pastikan script dijalankan sebagai root
+if [[ $EUID -ne 0 ]]; then
+   echo "Script ini harus dijalankan sebagai root!" 
+   exit 1
+fi
 
-echo "Mengonfigurasi jaringan dengan Netplan..."
+echo "Konfigurasi jaringan..."
 cat <<EOF > /etc/netplan/00-installer-config.yaml
+# This is the network config written by 'subiquity'
 network:
   ethernets:
     enp0s3:
@@ -21,16 +25,22 @@ network:
 EOF
 
 netplan apply
-sleep 2
 
-echo "Update dan install paket yang diperlukan..."
+echo "Update dan instal BIND9..."
 apt update -y
-apt install -y bind9 apache2 php php-mysql php-cli php-cgi php-gd mariadb-server unzip -y
+apt-get install bind9 -y
 
-echo "Mengonfigurasi BIND9..."
-cd /etc/bind
+echo "Konfigurasi zona DNS..."
+cat <<EOF > /etc/bind/named.conf.default-zones
+// prime the server with knowledge of the root servers
+zone "." {
+    type hint;
+    file "/usr/share/dns/root.hints";
+};
 
-cat <<EOF > named.conf.default-zones
+// be authoritative for the localhost forward and reverse zones, and for
+// broadcast zones as per RFC 1912
+
 zone "adrian.kasir" {
     type master;
     file "/etc/bind/db.domain";
@@ -40,54 +50,91 @@ zone "30.202.192.in-addr.arpa" {
     type master;
     file "/etc/bind/db.ip";
 };
+
+zone "0.in-addr.arpa" {
+    type master;
+    file "/etc/bind/db.0";
+};
+
+zone "255.in-addr.arpa" {
+    type master;
+    file "/etc/bind/db.255";
+};
 EOF
 
-cp db.local db.domain
-cat <<EOF > db.domain
-\$TTL 604800
-@   IN  SOA adrian.kasir. root.adrian.kasir. (
-        2
-        604800
-        86400
-        2419200
-        604800 )
-@   IN  NS adrian.kasir.
-@   IN  A 192.202.30.2
+cp /etc/bind/db.local /etc/bind/db.domain
+cat <<EOF > /etc/bind/db.domain
+;
+; BIND data file for local loopback interface
+;
+$TTL  604800
+@	IN	SOA	adrian.kasir. root.adrian.kasir. (
+			      2		; Serial
+			 604800		; Refresh
+			  86400		; Retry
+			2419200		; Expire
+			 604800	)	; Negative Cache TTL
+;
+@	IN	NS	adrian.kasir.
+@	IN	A	192.202.30.2
 EOF
 
-cp db.127 db.ip
-cat <<EOF > db.ip
-\$TTL 604800
-@   IN  SOA adrian.kasir. root.adrian.kasir. (
-        1
-        604800
-        86400
-        2419200
-        604800 )
-@   IN  NS adrian.kasir.
-2   IN  PTR adrian.kasir.
+cp /etc/bind/db.127 /etc/bind/db.ip
+cat <<EOF > /etc/bind/db.ip
+;
+; BIND data file for local loopback interface
+;
+$TTL  604800
+@   	IN  	SOA 	adrian.kasir. root.adrian.kasir. (
+        		     1		; Serial
+        		604800		; Refresh
+        		86400		; Retry
+        		2419200		; Expire
+        		604800 )	; Negative Cache TTL
+;
+@	IN  	NS 	adrian.kasir.
+2	IN  	PTR 	adrian.kasir.
 EOF
 
-systemctl restart bind9
+service bind9 restart
 
-echo "Menambahkan nameserver ke resolv.conf..."
+echo "Konfigurasi resolv.conf..."
 cat <<EOF > /etc/resolv.conf
 nameserver 192.202.30.2
 nameserver 192.202.30.1
 EOF
 
-echo "Mengonfigurasi database..."
-mysql -u root <<EOF
-CREATE DATABASE db_kasir;
+echo "Test DNS dengan nslookup..."
+nslookup adrian.kasir
+
+echo "Instalasi Apache, PHP, dan MariaDB..."
+apt-get install apache2 php php-{mysql,cli,cgi,gd} mariadb-server unzip -y
+
+echo "Konfigurasi database..."
+mysql_secure_installation <<EOF
+
+Y
+Y
+123
+123
+Y
+Y
+Y
+Y
 EOF
 
-echo "Mengunduh aplikasi web..."
+echo "Membuat database db_kasir..."
+mysql -e "CREATE DATABASE db_kasir;"
+
+echo "Download dan konfigurasi aplikasi POS..."
 cd /var/www/html
+rm -f index.html
 wget https://fnoor.my.id/app/pos.zip
 unzip pos.zip
 mysql db_kasir < db_toko.sql
 
-cat <<EOF > config.php
+echo "Konfigurasi file PHP..."
+cat <<EOF > /var/www/html/config.php
 <?php
 date_default_timezone_set("Asia/Jakarta");
 error_reporting(0);
@@ -99,13 +146,15 @@ error_reporting(0);
 
 try {
     \$config = new PDO("mysql:host=\$host;dbname=\$dbname", \$user, \$pass);
-} catch(PDOException \$e) {
+} catch (PDOException \$e) {
     echo "KONEKSI GAGAL: " . \$e->getMessage();
 }
+
 \$view = "fungsi/view/view.php";
 ?>
 EOF
 
+echo "Mengatur izin direktori web..."
 chown -R www-data:www-data /var/www/html/
 
-echo "Konfigurasi selesai!"
+echo "Setup selesai! Silakan cek konfigurasi Anda."
